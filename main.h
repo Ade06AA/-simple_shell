763de2203917d515fc4c316cadba07c8bccc6aa4
Danjumajamiu <jamiudanju1@gmail.com>
2023-05-26 09:48:51 -0700

    updated

--- main.h
+++ main.h
@@ -15,7 +15,7 @@ void myfree(char **);
 void fr(char *, char **);
 char **mystrtok(char *, char);
 
-/* sub2 */ 
+/* sub2 */
 size_t mywrite(char *, int);
 int mybuiltin(char **, char **);
 int mycheck(char **);
@@ -32,69 +32,4 @@ void myjoin(char **, char *);
 void mycheck2(char **);
 void prompt(int);
 int myexit2(char **, char *, int, int *, int *);
-
- /* get PATH from enviromental variable
-* @e: func arg 1
- * enviromental variable
-* Return: PATH (char *)
-*/
-/*char *getpath(char **e)
-{
-	int i;
-	for (i = 0; e[i][0] != 'P' || e[i][1] != 'A' || e[i][2] != 'T' || e[i][3] != 'H'; i++)
-		;
-	return (e[i]);
-}*/
-
-
-/*	char *c;*/
-/*	int i;*/
-
-/*	for (i = 0; i != 100; i++)*/
-/*		c[i] = a[i];*/
-/*	c[i] = '\0';*/
-/*	return (c);*/
-/*}*/
-
-/**
-* mypathexec - func name
- * handels command that are found in PATH
-* @tok: func arg 1
- * the tokenized input
-* @a: func arg 2
- * the environment variables whic also contain PATH
-* Return: 1 if the command is found in PATH else 0
-*/
-/*int mypathexec(char **tok, char **a)
-{
-	int i, j;
-	char **token;
-	char *path;
-
-	path = malloc(200);
-	path = getpath(a);
-	for (i = 0; path[i] != '='; ++i)
-		;
-	i++;
-	token = mystrtok(path + i, ':');
-	for (i = 0; token[i] != NULL && (token[i][0] != 'P' && token[i][2] != 'O'); i++)
-	{
-		myjoin(&token[i], tok[0]);
-		if (!(access(token[i], F_OK)))
-		{
-			j = fork();
-			if (j == 0)
-			{
-				execve(token[i], tok, NULL);
-				return (0);
-			}
-			else
-			{
-				wait(NULL);
-				return (1);
-			}
-		}
-	}
-	return (0);
-}*/
 #endif
